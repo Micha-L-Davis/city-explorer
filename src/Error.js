887c854569a1afcd2aefbd541ede19ca2c99dc09
Micha Davis <m.leigh.davis@gmail.com>
2022-02-21 17:54:49 -0800

    add error handling, apply style

---
+++ src/Error.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { ModalBody } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal"
+
+class Error extends React.Component {
+    render(){
+        return(
+            <Modal.Dialog>
+                <Modal.Header closeButton>
+                    <Modal.Title>Error!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>The request you submitted is invalid. Please enter a <em>real world</em> place name and ensure it is spelled correctly.</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <p color="gray">{this.props.errorMessage}</p>
+                </Modal.Footer>
+            </Modal.Dialog>
+        )
+    }
+}
+
+export default Error;
--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Form, Button, Image, ListGroup } from "react-bootstrap";
+import { Form, Button, Image, ListGroup, Modal, Row, Col } from "react-bootstrap";
 import axios from "axios";
+import Error from "./Error"
 
 class Main extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       searchRequest: '',
-      searchResults: []
+      searchResults: [],
+      errorMessage: '',
+      showModal: false
     }
   }
 
+  handleCloseModal = () => this.setState({ showModal: false })
 
   handleSearchRequest = (event) => {
     this.setState({searchRequest: event.target.value})
@@ -18,40 +22,56 @@ class Main extends React.Component {
 
   handleFetchCityData = async (event) => {
     event.preventDefault();
+    try{
+      let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.searchRequest}&format=json`
+      let cityData = await axios.get(url);
+      let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityData.data[0].lat},${cityData.data[0].lon}&zoom=13`
 
-    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.searchRequest}&format=json`
-    let cityData = await axios.get(url);
-    let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityData.data[0].lat},${cityData.data[0].lon}&zoom=13`
-
-    this.setState({
-      searchResults: [
-        <Image 
-          roundedCircle={true} 
-          src={mapURL} 
-          alt={`A map of ${cityData.data[0].display_name}`} 
-          width={400}
-          height={400}
-        />,
-        <ListGroup.Item>{`City: ${cityData.data[0].display_name}`}</ListGroup.Item>,
-        <ListGroup.Item>{`Latitude: ${cityData.data[0].lat}`}</ListGroup.Item>,
-        <ListGroup.Item>{`Longitude: ${cityData.data[0].lon}`}</ListGroup.Item>
-      ]
-    })
+      this.setState({
+        searchResults: [
+          <ListGroup.Item><Image 
+            roundedCircle={true} 
+            src={mapURL} 
+            alt={`A map of ${cityData.data[0].display_name}`} 
+            width={400}
+            height={400}
+          /></ListGroup.Item>,
+          <ListGroup.Item>{`City: ${cityData.data[0].display_name}`}</ListGroup.Item>,
+          <ListGroup.Item>{`Latitude: ${cityData.data[0].lat}`}</ListGroup.Item>,
+          <ListGroup.Item>{`Longitude: ${cityData.data[0].lon}`}</ListGroup.Item>
+        ]
+      })
+    }
+    catch (error){
+      console.log(error.response.data);
+      this.setState({
+        errorMessage: `${error.response.status}: ${error.response.data.error}`,
+        showModal: true
+      })
+    }
   }
 
   render() {
     return (
       <main>
         <Form onSubmit={this.handleFetchCityData}>
-          <Form.Group>
+          <Form.Group as={Row}>
             <Form.Label>Where will you explore?</Form.Label>
-            <Form.Control onChange={this.handleSearchRequest} type="text" name="search" id="input-search" placeholder="Enter a city name..." />
+            <Col sm={9}>
+              <Form.Control onChange={this.handleSearchRequest} type="text" name="search" id="input-search" placeholder="Enter a city name..." />
+            </Col>
+            <Col>
+              <Button variant="info" type="submit" >Explore!</Button>
+            </Col>
           </Form.Group>
-          <Button variant="info" type="submit" >Explore!</Button>
+          
         </Form>
         <ListGroup>
           {this.state.searchResults}
         </ListGroup>
+        <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
+          <Error errorMessage={this.state.errorMessage}/>
+        </Modal>
       </main>
     );
   }
